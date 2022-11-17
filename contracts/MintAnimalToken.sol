bbfade7280c1bbe08b549c56bf6c85f81d22fc46
Seung Hyo Lee <spinelee2002@gmail.com>
2022-11-17 10:50:27 +0000

    add: MintAnimnalToken.sol

---
+++ contracts/MintAnimalToken.sol
@@ -0,0 +1,16 @@
+// SPOX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+//민팅 함수 작성
+contract MintAnimalToken is ERC721Enumerable {
+    constructor() ERC721("HYOnimals", "HIM") {}
+
+    function mintAnimalToken() public {
+        uint256 animalTokenId = totalSupply() + 1;
+
+        _mint(msg.sender, animalTokenId);
+    }
+}
