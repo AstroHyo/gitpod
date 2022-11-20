f0e60b0ec95402dcce93a41c70578ba35a41d2e1
Seung Hyo Lee <spinelee2002@gmail.com>
2022-11-20 14:42:42 +0000

    add: frontend web3.js settting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,37 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from "./routes/main";
 
 const App: FC = () => {
+  //MetaMask 계정 가져와서 연결하기
+  const [account, setAccount] = useState<string>("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      } else {
+        alert("MetaMast를 설치하세요!");
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, []);
+
+  //잘 연결됐는지 확인
+  useEffect(() => {
+    console.log(account);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
