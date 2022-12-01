a367d70bc068049a190c2e21ce7620110ffcd925
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2022-12-01 23:32:43 +0900

    add: sale-animal tab

---
+++ frontend/src/routes/sale-animal.tsx
@@ -0,0 +1,11 @@
+import React, { FC, useEffect, useState } from "react";
+
+interface SaleAnimalProps {
+    account: string;
+}
+
+const SaleAnimal: FC<SaleAnimalProps> = ({ account }) => {
+    return <div>SA</div>;
+};
+
+export default SaleAnimal;
